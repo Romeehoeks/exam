14ab855a08b2bd6f93b70e39203e3b418eb3d6b2
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-04-05 11:50:48 +0200

    Update pivot_table.R

--- src/analysis/pivot_table.R
+++ src/analysis/pivot_table.R
@@ -2,6 +2,8 @@
 # CREATE PIVOT TABLE #
 ######################
 
+read.csv(file = "../../gen/dataprep/input/df_grouped.csv")
+
 # create pivot table
 df_pivot <- df_grouped %>% dcast(date ~ neighbourhood, fun.aggregate = sum, value.var = "num_reviews")
 
