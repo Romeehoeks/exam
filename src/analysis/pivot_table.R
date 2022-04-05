0a61caa4a4b583e6542ecb3549770eafef2ad40f
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-04-05 11:44:32 +0200

    Update pivot_table.R

--- src/analysis/pivot_table.R
+++ src/analysis/pivot_table.R
@@ -4,3 +4,5 @@
 
 # create pivot table
 df_pivot <- df_grouped %>% dcast(date ~ neighbourhood, fun.aggregate = sum, value.var = "num_reviews")
+
+write.csv(df_pivot, "../../gen/analysis/input/df_pivot", row.names = FALSE)
